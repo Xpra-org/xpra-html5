42c6f3dc10b0ac04100ce4ea6f180944e9250fda
totaam <antoine@xpra.org>
2022-01-25 21:14:08 +0700

    complete the separation of decode and paint

--- html5/js/OffscreenDecodeWorker.js
+++ html5/js/OffscreenDecodeWorker.js
@@ -22,16 +22,23 @@ importScripts("./RgbHelpers.js");
 // Array of offscreen canvases and decoders we have control over
 const offscreen_canvas = new Map();
 
+const image_coding = ["rgb", "rgb32", "rgb24", "jpeg", "png", "webp"];
+const video_coding = ["h264"];
+
 
 function decode_error(packet, start, error) {
+    packet[7] = null;
     self.postMessage({'error': ""+error, 'packet' : packet, 'start' : start});
 }
 
 function decode_ok(packet, start) {
+    packet[6] = "offscreen-painted";
+    packet[7] = null;
     self.postMessage({ 'draw': packet, 'start': start });
 }
 
-function paint_image(packet, start) {
+
+function paint_packet(packet, start) {
     const wid = packet[1],
         x = packet[2],
         y = packet[3],
@@ -40,7 +47,8 @@ function paint_image(packet, start) {
         coding = packet[6],
         data = packet[7];
 
-    let ctx = offscreen_canvas.get(wid)["ctx"];
+    const oc = offscreen_canvas.get(wid);
+    let ctx = oc["ctx"];
     if (coding == "bitmap") {
         // RGB is transformed to bitmap
         ctx.clearRect(x, y, width, height);
@@ -52,66 +60,50 @@ function paint_image(packet, start) {
         ctx.drawImage(data.image, x, y, width, height);
         data.image.close();
     }
-	else {
-		decode_error(packet, start, "unsupported encoding: "+coding);
-	}
-    // Replace the coding & drop data
-    packet[6] = "offscreen-painted";
-    packet[7] = null;
-	decode_ok(packet, start);
+    else if (coding == "scroll") {
+        const canvas = oc["c"];
+        for (let i = 0, j = data.length; i < j; ++i) {
+            const scroll_data = data[i];
+            const sx = scroll_data[0],
+                sy = scroll_data[1],
+                sw = scroll_data[2],
+                sh = scroll_data[3],
+                xdelta = scroll_data[4],
+                ydelta = scroll_data[5];
+            ctx.drawImage(canvas, sx, sy, sw, sh, sx + xdelta, sy + ydelta, sw, sh);
+        }
+    }
+    else if (coding == "frame") {
+        let enc_width = w;
+        let enc_height = h;
+        let options = packet[10] || {};
+        const scaled_size = options["scaled_size"];
+        if (scaled_size) {
+            enc_width = scaled_size[0];
+            enc_height = scaled_size[1];
+        }
+        ctx.drawImage(data, x, y, enc_width, enc_height);
+        data.close();
+    }
+    else if (coding == "throttle"){
+        //we are skipping this frame
+    }
+    else {
+        decode_error(packet, start, "unsupported video encoding: "+coding);
+    }
 }
+
 
 function new_image_decoder() {
     const image_decoder = new XpraImageDecoder();
-    image_decoder.on_frame_decoded = paint_image;
+    image_decoder.on_frame_decoded = packet_decoded;
     image_decoder.on_frame_error = decode_error;
     return image_decoder;
 }
 
-function paint_video_frame(packet, start) {
-    const wid = packet[1],
-        x = packet[2],
-        y = packet[3],
-        w = packet[4],
-        h = packet[5],
-        coding = packet[6],
-        data = packet[7];
-    let options = packet[10] || {};
-
-    let enc_width = w;
-    let enc_height = h;
-    const scaled_size = options["scaled_size"];
-    if (scaled_size) {
-        enc_width = scaled_size[0];
-        enc_height = scaled_size[1];
-    }
-
-    let ctx = offscreen_canvas.get(wid)["ctx"];
-    if (coding == "frame") {
-        ctx.drawImage(data, x, y, enc_width, enc_height);
-        data.close();
-        packet[6] = "offscreen-painted";
-        packet[7] = null;
-		decode_ok(packet, start);
-    }
-    else if (coding == "throttle"){
-        // Encoding throttle is used to slow down frame input
-        // TODO: Real error handling
-        const timeout = coding == "throttle" ? 500 : 0;
-        setTimeout(() => {
-            packet[6] = "offscreen-painted";
-            packet[7] = null;
-			decode_ok(packet, start);
-        }, timeout);
-    }
-	else {
-		decode_error(packet, start, "unsupported video encoding: "+coding);
-	}
-}
-
 function new_video_decoder() {
     const video_decoder = new XpraVideoDecoder();
-    video_decoder.on_frame_decoded = paint_video_frame;
+    video_decoder.on_frame_decoded = packet_decoded;
     video_decoder.on_frame_error = decode_error;
     return video_decoder;
 }
@@ -128,37 +120,35 @@ function add_decoders_for_window(wid, canvas) {
         });
 }
 
-function paint_scroll(packet, start) {
-    const wid = packet[1];
-    const data = packet[7];
-    const oc = offscreen_canvas.get(wid);
-    const canvas = oc["c"];
-    const ctx = oc["ctx"];
-    for (let i = 0, j = data.length; i < j; ++i) {
-        const scroll_data = data[i];
-        const sx = scroll_data[0],
-            sy = scroll_data[1],
-            sw = scroll_data[2],
-            sh = scroll_data[3],
-            xdelta = scroll_data[4],
-            ydelta = scroll_data[5];
-        ctx.drawImage(canvas, sx, sy, sw, sh, sx + xdelta, sy + ydelta, sw, sh);
+
+function packet_decoded(packet, start) {
+    //for now, paint immediately:
+    try {
+        paint_packet(packet, start);
+        const coding = packet[6];
+        if (coding == "throttle") {
+            // Encoding throttle is used to slow down frame input
+            const timeout = 500;
+            setTimeout(() => {
+                decode_ok(packet, start);
+            }, timeout);
+        }
+        else {
+            decode_ok(packet, start);
+        }
     }
-    packet[6] = "offscreen-painted";
-    packet[7] = null;
-	decode_ok(packet, start);
+    catch (e) {
+        decode_error(packet, start, e);
+    }
 }
 
-
 function decode_draw_packet(packet, start) {
-    const image_coding = ["rgb", "rgb32", "rgb24", "jpeg", "png", "webp"];
-    const video_coding = ["h264"];
     const wid = packet[1];
     const coding = packet[6];
     //const packet_sequence = packet[8];
     const oc = offscreen_canvas.get(wid);
     send_error = (message) => {
-        self.postMessage({'error': message, 'packet' : packet, 'start' : start});
+        decode_error(packet, start, message);
     }
     if (!oc) {
         send_error("no offscreen context for window "+wid);
@@ -166,28 +156,30 @@ function decode_draw_packet(packet, start) {
     }
 
     try {
-	    if (coding == "scroll") {
-			this.paint_scroll(packet, start);
-	    }
-	    else if (image_coding.includes(coding)) {
-	        // Add to image queue
-	        let decoder = oc["image-decoder"];
-	        decoder.queue_frame(packet, start);
-	    }
-	    else if (video_coding.includes(coding)) {
-	        // Add to video queue
-	        let decoder = oc["video-decoder"];
-	        if (!decoder.initialized) {
-	            // Init with width and heigth of this packet.
-	            // TODO: Use video max-size? It does not seem to matter.
-	            decoder.init(packet[4], packet[5]);
-	        }
-	        decoder.queue_frame(packet, start);
-	    }
-	    else {
-	        // We dont know, pass trough
-	        self.postMessage({ 'draw': packet, 'start': start });
-	    }
+        if (coding == "scroll") {
+            //nothing to do:
+            this.packet_decoded(packet, start);
+        }
+        else if (image_coding.includes(coding)) {
+            // Add to image queue
+            let decoder = oc["image-decoder"];
+            decoder.queue_frame(packet, start);
+        }
+        else if (video_coding.includes(coding)) {
+            // Add to video queue
+            let decoder = oc["video-decoder"];
+            if (!decoder.initialized) {
+                // Init with width and heigth of this packet.
+                // TODO: Use video max-size? It does not seem to matter.
+                decoder.init(packet[4], packet[5]);
+            }
+            decoder.queue_frame(packet, start);
+        }
+        else {
+            // We dont know, pass trough
+            // (this should not be happening?)
+            self.postMessage({ 'draw': packet, 'start': start });
+        }
     }
     catch (e) {
         send_error(e);
