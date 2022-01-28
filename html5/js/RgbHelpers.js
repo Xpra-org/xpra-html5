1622d484df8998677d3640410353b9d568bea913
totaam <antoine@xpra.org>
2022-01-28 16:04:50 +0700

    Javascript demands that rowstride=width*4 for rgb data
    make that more explicit

--- html5/js/RgbHelpers.js
+++ html5/js/RgbHelpers.js
@@ -4,7 +4,8 @@
 
 //deals with zlib or lz4 pixel compression
 //as well as converting rgb24 to rb32 and
-//re-striding the pixel data if needed
+//re-striding the pixel data if needed so that lines are not padded
+//(that is: the rowstride must be width*4)
 //this function modifies the packet data directly
 function decode_rgb(packet) {
 	const width = packet[4],
@@ -12,9 +13,7 @@ function decode_rgb(packet) {
 		coding = packet[6],
 		rowstride = packet[9];
 	let data = packet[7];
-	let options = {};
-	if (packet.length>10)
-		options = packet[10];
+	let options = packet[10] || {};
 	if (options["zlib"]>0) {
 		data = new Zlib.Inflate(data).decompress();
 		delete options["zlib"];
@@ -23,28 +22,27 @@ function decode_rgb(packet) {
 		data = lz4.decode(data);
 		delete options["lz4"];
 	}
-	let target_stride = width*4;
 	//this.debug("draw", "got ", data.length, "bytes of", coding, "to paint with stride", rowstride, ", target stride", target_stride);
 	if (coding=="rgb24") {
 		packet[9] = target_stride;
 		packet[6] = "rgb32";
-		return rgb24_to_rgb32(data, width, height, rowstride, target_stride);
+		return rgb24_to_rgb32(data, width, height, rowstride);
 	}
 	//coding=rgb32
-	if (target_stride==rowstride) {
+	if (rowstride==width*4) {
 		return new Uint8Array(data);
 	}
 	//re-striding
 	//might be quicker to copy 32bit at a time using Uint32Array
 	//and then casting the result?
-	const uint = new Uint8Array(target_stride*height);
+	const uint = new Uint8Array(width*height*4);
 	let i = 0,
 		j = 0,
 		psrc = 0,
 		pdst = 0;
 	for (i=0; i<height; i++) {
 		psrc = i*rowstride;
-		pdst = i*target_stride;
+		pdst = i*width*4;
 		for (j=0; j<target_stride; j++) {
 			uint[pdst++] = data[psrc++];
 		}
@@ -52,13 +50,13 @@ function decode_rgb(packet) {
 	return uint;
 }
 
-function rgb24_to_rgb32(data, width, height, rowstride, target_stride) {
-	const uint = new Uint8Array(target_stride*height);
+function rgb24_to_rgb32(data, width, height, rowstride) {
+	const uint = new Uint8Array(width*height*4);
 	let i = 0,
-		j = 0,
-		l = data.length;
+		j = 0;
 	if (rowstride==width*3) {
 		//faster path, single loop:
+		const l = l = data.length;
 		while (i<l) {
 			uint[j++] = data[i++];
 			uint[j++] = data[i++];
