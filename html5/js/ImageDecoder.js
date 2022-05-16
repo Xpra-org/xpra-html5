799330a2e432b106b57c6d88ccf508053b65e4cb
totaam <antoine@xpra.org>
2022-05-16 21:38:27 +0700

    #175 convert more code to ES6 classes

--- html5/js/ImageDecoder.js
+++ html5/js/ImageDecoder.js
@@ -15,41 +15,44 @@
  */
 
 const XpraImageDecoderLoader = {
-    hasNativeDecoder: function () {
-        return typeof ImageDecoder !== "undefined";
-    }
+	hasNativeDecoder: function () {
+		return typeof ImageDecoder !== "undefined";
+	}
 }
 
-function XpraImageDecoder() {
-    this.on_frame_decoded = null;
-    this.on_frame_error = (packet, error) => {
-	    console.error("ImageDecoder error on packet ", packet, ": ", error);
-    }
-}
+class XpraImageDecoder {
 
-XpraImageDecoder.prototype.queue_frame = function (packet) {
-    const width = packet[4];
-    const height = packet[5];
-    const coding = packet[6];
-    if (coding.startsWith("rgb")) {
-        // TODO: Figure out how to decode rgb with ImageDecoder API;
-        const data = decode_rgb(packet);
-        createImageBitmap(new ImageData(new Uint8ClampedArray(data.buffer), width, height), 0, 0, width, height).then((bitmap) => {
-            packet[6] = "bitmap:"+coding;
-            packet[7] = bitmap;
-            this.on_frame_decoded(packet);
-        }).catch(e => this.on_frame_error(packet, e));
-    } else {
-        const paint_coding = coding.split("/")[0];   //ie: "png/P" -> "png"
-        const decoder = new ImageDecoder({
-            type: "image/" + paint_coding,
-            data: packet[7],
-        });
-        decoder.decode({ frameIndex: 0 }).then((result) => {
-            packet[6] = "image:"+paint_coding;
-            packet[7] = result;
-            decoder.close();
-            this.on_frame_decoded(packet);
-        }).catch(e => this.on_frame_error(packet, e));
-    }
-};
+	constructor() {
+		this.on_frame_decoded = null;
+		this.on_frame_error = (packet, error) => {
+			console.error("ImageDecoder error on packet ", packet, ": ", error);
+		}
+	}
+
+	queue_frame = function (packet) {
+		const width = packet[4];
+		const height = packet[5];
+		const coding = packet[6];
+		if (coding.startsWith("rgb")) {
+			// TODO: Figure out how to decode rgb with ImageDecoder API;
+			const data = decode_rgb(packet);
+			createImageBitmap(new ImageData(new Uint8ClampedArray(data.buffer), width, height), 0, 0, width, height).then((bitmap) => {
+				packet[6] = "bitmap:"+coding;
+				packet[7] = bitmap;
+				this.on_frame_decoded(packet);
+			}).catch(e => this.on_frame_error(packet, e));
+		} else {
+			const paint_coding = coding.split("/")[0];   //ie: "png/P" -> "png"
+			const decoder = new ImageDecoder({
+				type: "image/" + paint_coding,
+				data: packet[7],
+			});
+			decoder.decode({ frameIndex: 0 }).then((result) => {
+				packet[6] = "image:"+paint_coding;
+				packet[7] = result;
+				decoder.close();
+				this.on_frame_decoded(packet);
+			}).catch(e => this.on_frame_error(packet, e));
+		}
+	}
+}
--- html5/js/VideoDecoder.js
+++ html5/js/VideoDecoder.js
@@ -21,140 +21,143 @@ const XpraVideoDecoderLoader = {
     }
 }
 
-function XpraVideoDecoder() {
-    this.initialized = false;
-    this.had_first_key = false;
-    this.draining = false;
+class XpraVideoDecoder {
 
-    this.decoder_queue = [];
-    this.frame_threshold = 250;
-    this.on_frame_decoded = {}; //callback
-    this.on_frame_error = (packet, error) => {
-        console.error("VideoDecoder error on packet ", packet, ": ", error);
-    }
+	constructor() {
+	    this.initialized = false;
+	    this.had_first_key = false;
+	    this.draining = false;
+
+	    this.decoder_queue = [];
+	    this.frame_threshold = 250;
+	    this.on_frame_decoded = {}; //callback
+	    this.on_frame_error = (packet, error) => {
+	        console.error("VideoDecoder error on packet ", packet, ": ", error);
+	    }
+	}
+
+	init(width, height) {
+	    this.videoDecoder = new VideoDecoder({
+	        output: this._on_decoded_frame.bind(this),
+	        error: this._on_decoder_error.bind(this)
+	    });
+
+	    this.videoDecoder.configure({
+	        codec: "avc1.42C01E",
+	        // hardwareAcceleration: "prefer-hardware",
+	        optimizeForLatency: true,
+	        codedWidth: width,
+	        codedHeight: height
+	    });
+	    this.last_timestamp = 0;
+	    this.initialized = true;
+	}
+
+	_on_decoded_framefunction (videoFrame) {
+	    if (this.decoder_queue.length == 0) {
+	        videoFrame.close();
+	        return;
+	    }
+
+	    // Find the frame
+	    const frame_timestamp = videoFrame.timestamp;
+	    let current_frame = this.decoder_queue.filter(q => q.p[10]["frame"] == frame_timestamp);
+	    if (current_frame.length == 1) {
+	        // We found our frame!
+	        this.decoder_queue = this.decoder_queue.filter(q => q.p[10]["frame"] != frame_timestamp);
+	        current_frame = current_frame[0];
+	    } else {
+	        // We decoded a frame the is no longer queued??
+	        // TODO: handle error??
+	        videoFrame.close();
+	        return;
+	    }
+
+	    if (frame_timestamp == 0) {
+	        this.last_timestamp = 0;
+	    }
+
+	    if ((this.decoder_queue.length > this.frame_threshold) || this.last_timestamp > frame_timestamp) {
+	        // Skip if the decoders queue is growing too big or this frames timestamp is smaller then the last one painted.
+	        videoFrame.close();
+
+	        const packet = current_frame.p;
+	        packet[6] = "throttle";
+	        packet[7] = null;
+	        this.on_frame_decoded(packet);
+	        return;
+	    }
+
+	    this.last_timestamp = frame_timestamp;
+	    const packet = current_frame.p;
+
+	    // Latest possible check for draining
+	    if (this.draining) {
+	        videoFrame.close();
+	        return;
+	    }
+
+	    packet[6] = "frame:"+packet[6];
+	    packet[7] = videoFrame;
+	    this.on_frame_decoded(packet, current_frame);
+	}
+
+	_on_decoder_error(err) {
+	    // TODO: Handle err? Or just assume we will catch up?
+	    this._close();
+	}
+
+	queue_frame(packet) {
+	    let options = packet[10] || {};
+	    const data = packet[7];
+	    decode_error = (error) => {
+	        this.on_frame_error(packet, error);
+	    }
+
+	    if (!this.had_first_key && options["type"] != "IDR" )
+	    {
+	        decode_error("first frame must be a key frame");
+	        return;
+	    }
+
+	    if (this.videoDecoder.state == "closed") {
+	        decode_error("video decoder is closed");
+	        return;
+	    }
+	    if (this.draining) {
+	        decode_error("video decoder is draining");
+	        return;
+	    }
+
+	    this.had_first_key = true;
+	    this.decoder_queue.push({"p" : packet});
+	    const init = {
+	        type: options["type"] == "IDR" ? "key" : "delta",
+	        data: data,
+	        timestamp: options["frame"],
+	    };
+	    const chunk = new EncodedVideoChunk(init);
+	    this.videoDecoder.decode(chunk);
+	}
+
+	_close() {
+	    if (this.initialized) {
+	        if (this.videoDecoder.state != "closed") {
+	            this.videoDecoder.close();
+	        }
+	        this.had_first_key = false;
+
+	        // Callback on all frames (bail out)
+	        this.draining = true;
+	        let drain_queue = this.decoder_queue;
+	        this.decoder_queue = [];
+
+	        for (let frame of drain_queue) {
+	            const packet = frame.p;
+	            this.on_frame_error(packet, "video decoder is draining");
+	        };
+	        this.draining = false;
+	    }
+	    this.initialized = false;
+	}
 }
-
-XpraVideoDecoder.prototype.init = function (width, height) {
-    this.videoDecoder = new VideoDecoder({
-        output: this._on_decoded_frame.bind(this),
-        error: this._on_decoder_error.bind(this)
-    });
-
-    this.videoDecoder.configure({
-        codec: "avc1.42C01E",
-        // hardwareAcceleration: "prefer-hardware",
-        optimizeForLatency: true,
-        codedWidth: width,
-        codedHeight: height
-    });
-    this.last_timestamp = 0;
-    this.initialized = true;
-};
-
-XpraVideoDecoder.prototype._on_decoded_frame = function (videoFrame) {
-    if (this.decoder_queue.length == 0) {
-        videoFrame.close();
-        return;
-    }
-
-    // Find the frame
-    const frame_timestamp = videoFrame.timestamp;
-    let current_frame = this.decoder_queue.filter(q => q.p[10]["frame"] == frame_timestamp);
-    if (current_frame.length == 1) {
-        // We found our frame!
-        this.decoder_queue = this.decoder_queue.filter(q => q.p[10]["frame"] != frame_timestamp);
-        current_frame = current_frame[0];
-    } else {
-        // We decoded a frame the is no longer queued??
-        // TODO: handle error??
-        videoFrame.close();
-        return;
-    }
-
-    if (frame_timestamp == 0) {
-        this.last_timestamp = 0;
-    }
-
-    if ((this.decoder_queue.length > this.frame_threshold) || this.last_timestamp > frame_timestamp) {
-        // Skip if the decoders queue is growing too big or this frames timestamp is smaller then the last one painted.
-        videoFrame.close();
-
-        const packet = current_frame.p;
-        packet[6] = "throttle";
-        packet[7] = null;
-        this.on_frame_decoded(packet);
-        return;
-    }
-
-    this.last_timestamp = frame_timestamp;
-    const packet = current_frame.p;
-
-    // Latest possible check for draining
-    if (this.draining) {
-        videoFrame.close();
-        return;
-    }
-
-    packet[6] = "frame:"+packet[6];
-    packet[7] = videoFrame;
-    this.on_frame_decoded(packet, current_frame);
-}
-
-XpraVideoDecoder.prototype._on_decoder_error = function (err) {
-    // TODO: Handle err? Or just assume we will catch up?
-    this._close();
-};
-
-XpraVideoDecoder.prototype.queue_frame = function (packet) {
-    let options = packet[10] || {};
-    const data = packet[7];
-    decode_error = (error) => {
-        this.on_frame_error(packet, error);
-    }
-
-    if (!this.had_first_key && options["type"] != "IDR" )
-    {
-        decode_error("first frame must be a key frame");
-        return;
-    }
-
-    if (this.videoDecoder.state == "closed") {
-        decode_error("video decoder is closed");
-        return;
-    }
-    if (this.draining) {
-        decode_error("video decoder is draining");
-        return;
-    }
-
-    this.had_first_key = true;
-    this.decoder_queue.push({"p" : packet});
-    const init = {
-        type: options["type"] == "IDR" ? "key" : "delta",
-        data: data,
-        timestamp: options["frame"],
-    };
-    const chunk = new EncodedVideoChunk(init);
-    this.videoDecoder.decode(chunk);
-};
-
-XpraVideoDecoder.prototype._close = function () {
-    if (this.initialized) {
-        if (this.videoDecoder.state != "closed") {
-            this.videoDecoder.close();
-        }
-        this.had_first_key = false;
-
-        // Callback on all frames (bail out)
-        this.draining = true;
-        let drain_queue = this.decoder_queue;
-        this.decoder_queue = [];
-
-        for (let frame of drain_queue) {
-            const packet = frame.p;
-            this.on_frame_error(packet, "video decoder is draining");
-        };
-        this.draining = false;
-    }
-    this.initialized = false;
-};
