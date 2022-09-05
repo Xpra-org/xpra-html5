ecbcf966e825a45c06af3f0f51bf6ca185e8368a
TijZwa <tijzwa@vpo.nl>
2022-09-05 10:35:37 +0200

    Move paint, add css class

--- html5/js/OffscreenDecodeWorker.js
+++ html5/js/OffscreenDecodeWorker.js
@@ -8,7 +8,7 @@
  */
 
 /*
- * Worker for offscreen decoding and painting.
+ * Worker for offscreen decoding.
  */
 
 importScripts("./lib/lz4.js");
@@ -19,7 +19,7 @@ importScripts("./RgbHelpers.js");
 importScripts("./Constants.js");
 
 // WindowDecoder for each window we have control over:
-const offscreen_canvas = new Map();
+const window_decoders = new Map();
 
 const image_coding = [
   "rgb",
@@ -56,11 +56,23 @@ function send_decode_error(packet, error) {
   self.postMessage({ error: `${error}`, packet });
 }
 
+const paint_worker = new Worker("PaintWorker.js");
+
 class WindowDecoder {
-  constructor(canvas, debug) {
-    this.canvas = canvas;
+  constructor(wid, canvas, debug) {
+    this.wid = wid;
+
+    paint_worker.postMessage(
+      {
+        cmd: "canvas",
+        wid,
+        canvas,
+        debug
+      },
+      [canvas]
+    );
+
     this.debug = debug;
-    this.still = new OffscreenCanvas(this.canvas.width, this.canvas.height);
     this.init();
   }
   init() {
@@ -132,92 +144,22 @@ class WindowDecoder {
     } else {
       this.decode_error(packet, `unsupported encoding: '${coding}'`);
     }
-    // Paint the packet on screen refresh (if we can use requestAnimationFrame in the worker)
-    if (typeof requestAnimationFrame == "function") {
-      requestAnimationFrame(() => {
-        this.paint_packet(packet, start);
-      });
-    } else {
-      console.warn(
-        "No function requestAnimationFrame in webworkers supported by this browser."
-      );
-      this.paint_packet(packet, start);
-    }
-  }
 
-  paint_packet(packet, start) {
-    // Update the coding propery
-    const coding = packet[6];
-    const x = packet[2];
-    const y = packet[3];
-    const width = packet[4];
-    const height = packet[5];
-    const data = packet[7];
-
-    let context = this.canvas.getContext("2d");
-    if (coding.startsWith("bitmap")) {
-      // Bitmap paint
-      context.imageSmoothingEnabled = false;
-      context.clearRect(x, y, width, height);
-      context.drawImage(data, x, y, width, height);
-      this.paint_box(coding, context, x, y, width, height);
-    } else if (coding == "scroll") {
-      context.imageSmoothingEnabled = false;
-      for (let index = 0, stop = data.length; index < stop; ++index) {
-        const scroll_data = data[index];
-        const sx = scroll_data[0];
-        const sy = scroll_data[1];
-        const sw = scroll_data[2];
-        const sh = scroll_data[3];
-        const xdelta = scroll_data[4];
-        const ydelta = scroll_data[5];
-        context.drawImage(
-          this.canvas,
-          sx,
-          sy,
-          sw,
-          sh,
-          sx + xdelta,
-          sy + ydelta,
-          sw,
-          sh
-        );
-        this.paint_box(coding, context, sx, sy, sw, sh);
-      }
-    } else if (coding.startsWith("frame")) {
-      context.drawImage(data, x, y, width, height);
-      data.close();
-      this.paint_box(coding, context, x, y, width, height);
-    }
-
-    // Decode ok.
+    // Fake packet to send back
     const options = packet[10] || {};
     const decode_time = Math.round(1000 * (performance.now() - start));
     options["decode_time"] = Math.max(0, decode_time);
-    packet[6] = "offscreen-painted";
-    packet[7] = null;
-    packet[10] = options;
-    self.postMessage({ draw: packet, start });
+    // Copy without data
+    const clonepacket = packet.map((x, i) => { if (i != 7) { return x; } });
+    clonepacket[6] = "offscreen-painted";
+    clonepacket[10] = options;
 
-    // Call update_still in callback
-    setTimeout(() => this.update_still(), 0);
-  }
+    // Tell the server we are done with this packet
+    self.postMessage({ draw: clonepacket, start });
 
-  paint_box(coding, context, px, py, pw, ph) {
-    if (!this.debug) {
-      return;
-    }
-    const source_encoding = coding.split(":")[1] || ""; //ie: "rgb24"
-    const box_color = DEFAULT_BOX_COLORS[source_encoding];
-    if (box_color) {
-      context.strokeStyle = box_color;
-      context.lineWidth = 2;
-      context.strokeRect(px, py, pw, ph);
-    }
-  }
+    // Paint the packet on screen refresh (if we can use requestAnimationFrame in the worker)
+    paint_worker.postMessage({ cmd: "paint", image: packet[7], wid: packet[1], coding: packet[6], x: packet[2], y: packet[3], w: packet[4], h: packet[5] }, [packet[7]]);
 
-  update_still() {
-    this.still.getContext("2d").drawImage(this.canvas, 0, 0);
   }
 
   eos() {
@@ -227,27 +169,13 @@ class WindowDecoder {
     this.decode_queue.push(packet);
   }
 
-  redraw() {
-    // Redraw the last know state (saved on still)
-    this.canvas.getContext("2d").drawImage(this.still, 0, 0);
-  }
-
-  update_geometry(w, h) {
-    if (this.closed) {
-      return;
-    }
-    if (this.canvas.width == w && this.canvas.height == h) {
-      //unchanged
-      return;
-    }
-    this.canvas.width = w;
-    this.canvas.height = h;
-    // Also update the still
-    this.still.width = w;
-    this.still.height = h;
-  }
-
   close() {
+    paint_worker.postMessage(
+      {
+        cmd: "remove",
+        wid: this.wid
+      }
+    );
     if (!this.closed) {
       this.closed = true;
       this.eos();
@@ -267,39 +195,41 @@ onmessage = function (e) {
       break;
     }
     case "eos":
-      wd = offscreen_canvas.get(data.wid);
+      wd = window_decoders.get(data.wid);
       if (wd) {
         wd.eos();
       }
       break;
     case "remove":
-      wd = offscreen_canvas.get(data.wid);
+      wd = window_decoders.get(data.wid);
       if (wd) {
         wd.close();
-        offscreen_canvas.delete(data.wid);
+        window_decoders.delete(data.wid);
       }
       break;
     case "decode": {
       const packet = data.packet;
       const wid = packet[1];
-      wd = offscreen_canvas.get(wid);
+      wd = window_decoders.get(wid);
       if (wd) {
         wd.queue_draw_packet(packet);
       } else {
         send_decode_error(
           packet,
           `no window decoder found for wid ${wid}, only:${[
-            ...offscreen_canvas.keys(),
+            ...window_decoders.keys(),
           ].join(",")}`
         );
       }
       break;
     }
     case "redraw":
-      wd = offscreen_canvas.get(data.wid);
-      if (wd) {
-        wd.redraw();
-      }
+      paint_worker.postMessage(
+        {
+          cmd: data.cmd,
+          wid: data.wid
+        }
+      );
       break;
     case "canvas":
       console.log(
@@ -310,23 +240,21 @@ onmessage = function (e) {
         data.debug
       );
       if (data.canvas) {
-        offscreen_canvas.set(
+        window_decoders.set(
           data.wid,
-          new WindowDecoder(data.canvas, data.debug)
+          new WindowDecoder(data.wid, data.canvas, data.debug)
         );
       }
       break;
     case "canvas-geo":
-      wd = offscreen_canvas.get(data.wid);
-      if (wd) {
-        wd.update_geometry(data.w, data.h);
-      } else {
-        console.warn(
-          "cannot update canvas geometry, window",
-          data.wid,
-          "not found"
-        );
-      }
+      paint_worker.postMessage(
+        {
+          cmd: data.cmd,
+          w: data.w,
+          h: data.h,
+          wid: data.wid
+        }
+      );
       break;
     default:
       console.error(`Offscreen decode worker got unknown message: ${data.cmd}`);
