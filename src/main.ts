fc077589dce549c2b4878f93a349944704954987
Andrew G. Knackstedt <andrew.knackstedt@dynatrace.com>
2022-11-17 19:47:46 -0500

    Add main TS entrypoint

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+
+window.addEventListener("load", () => {
+    // Register ServiceWorker.
+    navigator.serviceWorker
+        ?.register("service-worker.js")
+        .then((registration) => {
+            console.log("Service Worker registered: ", registration);
+        })
+        .catch((registrationError) => {
+            console.error("Service Worker registration failed: ", registrationError);
+        });
+});
