54f927252a887c6d31a7ee4bf4523e39444aad33
Andrew G. Knackstedt <andrew.knackstedt@dynatrace.com>
2022-11-22 22:35:57 -0500

    Remove util object

--- src/app/lib/lz4.ts
+++ src/app/lib/lz4.ts
@@ -10,11 +10,10 @@
 
 
 /********************************************************************************/
-util = {}
 
 // Simple hash function, from: http://burtleburtle.net/bob/hash/integer.html.
 // Chosen because it doesn't use multiply and achieves full avalanche.
-util.hashU32 = function hashU32(a) {
+const hashU32 = function hashU32(a) {
     a = a | 0;
     a = a + 2127912214 + (a << 12) | 0;
     a = a ^ -949894596 ^ a >>> 19;
@@ -25,7 +24,7 @@ util.hashU32 = function hashU32(a) {
 }
 
 // Reads a 64-bit little-endian integer from an array.
-util.readU64 = function readU64(b, n) {
+const readU64 = function readU64(b, n) {
     var x = 0;
     x |= b[n++] << 0;
     x |= b[n++] << 8;
@@ -39,7 +38,7 @@ util.readU64 = function readU64(b, n) {
 }
 
 // Reads a 32-bit little-endian integer from an array.
-util.readU32 = function readU32(b, n) {
+const readU32 = function readU32(b, n) {
     var x = 0;
     x |= b[n++] << 0;
     x |= b[n++] << 8;
@@ -49,7 +48,7 @@ util.readU32 = function readU32(b, n) {
 }
 
 // Writes a 32-bit little-endian integer from an array.
-util.writeU32 = function writeU32(b, n, x) {
+const writeU32 = function writeU32(b, n, x) {
     b[n++] = (x >> 0) & 0xff;
     b[n++] = (x >> 8) & 0xff;
     b[n++] = (x >> 16) & 0xff;
@@ -58,7 +57,7 @@ util.writeU32 = function writeU32(b, n, x) {
 
 // Multiplies two numbers using 32-bit integer multiplication.
 // Algorithm from Emscripten.
-util.imul = function imul(a, b) {
+const imul = function imul(a, b) {
     var ah = a >>> 16;
     var al = a & 65535;
     var bh = b >>> 16;
