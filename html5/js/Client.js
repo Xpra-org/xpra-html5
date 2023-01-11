ed4b0d72f40864cea4fb4b91b5c400085eb44fa8
TijZwa <tijzwa@vpo.nl>
2023-01-11 11:07:40 +0100

    Less scaling for offscreen decoding

--- html5/js/Client.js
+++ html5/js/Client.js
@@ -1527,6 +1527,11 @@ class XpraClient {
         "encoding.scrolling.preference": 20,
       });
     }
+  let video_max_size = [1024, 768];
+  if (this.offscreen_api) {
+    video_max_size = [4096, 4096];
+  }
+
     this._update_capabilities({
       auto_refresh_delay: 500,
       randr_notify: true,
@@ -1570,7 +1575,7 @@ class XpraClient {
       //video stuff:
       "encoding.color-gamut": Utilities.getColorGamut(),
       "encoding.video_scaling": true,
-      "encoding.video_max_size": [1024, 768],
+      "encoding.video_max_size": video_max_size,
       "encoding.eos": true,
       "encoding.full_csc_modes": {
         mpeg1: ["YUV420P"],
