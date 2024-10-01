a0256fc3a43a18ea30a56cbeaac81d3dc7023c16
Antoine Martin <totaam@xpra.org>
2024-10-01 22:13:49 +0700

    check for rencodeplus as early as possible

--- html5/js/Client.js
+++ html5/js/Client.js
@@ -2467,6 +2467,10 @@ class XpraClient {
     this.cancel_hello_timer();
     const hello = packet[1];
     this.clog("received hello capabilities", hello);
+    if (!hello["rencodeplus"]) {
+      throw "no common packet encoders, 'rencodeplus' is required by this client";
+    }
+
     this.server_display = hello["display"] || "";
     this.server_platform = hello["platform"] || "";
     this.server_remote_logging = hello["remote-logging.multi-line"];
@@ -2503,9 +2507,6 @@ class XpraClient {
       }
       console.info("cipher out caps=", JSON.stringify(this.cipher_out_caps));
       this.protocol.set_cipher_out(this.cipher_out_caps, this.encryption_key);
-    }
-    if (!hello["rencodeplus"]) {
-      throw "no common packet encoders, 'rencodeplus' is required by this client";
     }
 
     const version = Utilities.s(hello["version"]);
