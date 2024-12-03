7dd865db033769e505c6c5bb5fb72f0644171a97
Antoine Martin <totaam@xpra.org>
2024-12-03 22:01:49 +0700

    useful debugging function

--- html5/js/Utilities.js
+++ html5/js/Utilities.js
@@ -103,6 +103,15 @@ const Utilities = {
     return hex;
   },
 
+  hexToString(hexval) {
+    var hex = hexval.toString();
+    var str = '';
+    for (var i = 0; i < hex.length; i += 2) {
+        str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
+    }
+    return str;
+  },
+
   arrayToHex(buffer) {
     return Array.prototype.map.call(buffer, x => ('00' + x.toString(16)).slice(-2)).join('');
   },
